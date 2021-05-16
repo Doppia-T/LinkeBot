a01656d17095d6ec7b2062ef4a379cbce089ce46
Doppia-T <64402378+Doppia-T@users.noreply.github.com>
2021-05-16 21:55:55 +0200

    Update and rename LinkeBot_login.py to LinkeBot_core.py

--- LinkeBot/LinkeBot_core.py
+++ LinkeBot/LinkeBot_core.py
@@ -1,11 +1,12 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 import time
 
 
-#does login with credential taken from external file (more actions in the future)
-class LinkeBot_login:
+# does login with credential taken from external file (more actions in the future)
+class LinkeBot_BOT:
     def __init__(self,username,password):
         self.username = username
         self.password = password
@@ -23,3 +24,12 @@ class LinkeBot_login:
         username.send_keys(self.username)
         password.send_keys(self.password)
         password.send_keys(Keys.RETURN)
+        time.sleep(2)
+
+    def search_LinkePage(self, LinkeGetTarget):
+        bot = self.bot
+
+        import LinkeBot_import_target
+        LinkeGetTarget = LinkeBot_import_target.get_LinkeTarget()
+        bot.get(('https://www.linkedin.com/search/results/all/?keywords=' + LinkeGetTarget + '&origin=GLOBAL_SEARCH_HEADER'))
+        time.sleep(2)
