ce9e7e331057ca1c286a6e8eb84cd026790ddfdb
Doppia-T <64402378+Doppia-T@users.noreply.github.com>
2021-05-16 21:54:58 +0200

    Update LinkeBot_main.py

--- LinkeBot/LinkeBot_main.py
+++ LinkeBot/LinkeBot_main.py
@@ -1,13 +1,26 @@
-import LinkeBot_import_credentials
-from LinkeBot_login import LinkeBot_login
 
-#starts process of getting login credentials
+from LinkeBot_core import LinkeBot_BOT
+
+# ANCILLARY 
+# starts process of getting login credentials
+import LinkeBot_import_credentials
+
 LinkeUsername = LinkeBot_import_credentials.get_LinkeID()
 LinkePassword = LinkeBot_import_credentials.get_LinkePSS()
 
-#starts process of logging in with the credentials previously obtained
-LinkeBot = LinkeBot_login(LinkeUsername,LinkePassword)
-LinkeBot.login()
+# PRIMARY
+# starts process of logging in with the credentials previously obtained
+LinkeBot_login = LinkeBot_BOT(LinkeUsername, LinkePassword)
+LinkeBot_login.login()
 
-# TEST
-# print(LinkeUsername,LinkePassword)
+# ANCILLARY
+# starts process of getting a page or a profile (target) to search on LinkedIn
+import LinkeBot_import_target
+
+LinkeGetTarget = LinkeBot_import_target.get_LinkeTarget()
+
+# PRIMARY
+# start process of searching a page or a profile with the search tool
+# of LinkedIn
+LinkeBot_search = LinkeBot_BOT(LinkeUsername, LinkePassword)
+LinkeBot_search.search_LinkePage(LinkeGetTarget)
