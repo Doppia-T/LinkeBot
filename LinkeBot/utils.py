7e37fea33edd12ce2ee9b0cbcbbb2b1e6d36e203
Anyesh <sthacruz@gmail.com>
2021-07-13 11:52:41 +0545

    feat: Add yaml loader

--- LinkeBot/utils.py
+++ LinkeBot/utils.py
@@ -1,7 +1,19 @@
+import logging
 import random
 import time
+
+import yaml
 
 
 def random_nap():
     nap_time = random.randint(30, 300)
     time.sleep(nap_time)
+
+
+def yaml_loader(yaml_path):
+    with open(yaml_path, "r") as d:
+        try:
+            return yaml.safe_load(d)
+        except yaml.YAMLError as exc:
+            logging.error(f"There was an error {exc}")
+            return None
