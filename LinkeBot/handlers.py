80b73877b06f269de263a79759861e0a3b1ddfda
Anyesh <sthacruz@gmail.com>
2021-07-13 11:55:24 +0545

    fix: Update with utils

--- LinkeBot/handlers.py
+++ LinkeBot/handlers.py
@@ -1,7 +1,6 @@
-import logging
 from typing import Tuple
 
-import yaml
+from linkebot.utils import yaml_loader
 
 from .settings import CONFIGDIR
 
@@ -10,21 +9,14 @@ def get_linkedin_creds() -> Tuple[str, str]:
     # gets username from an external file called "credentials"
     email, password = ("", "")
 
-    with open(CONFIGDIR / "credentials.yaml", "r") as creds:
-        try:
-            creds = yaml.safe_load(creds)
-            email = creds.get("email", None)  # type: ignore
-            password = creds.get("password", None)  # type: ignore
-        except yaml.YAMLError as exc:
-            logging.error(f"There was an error {exc}")
+    creds = yaml_loader(CONFIGDIR / "credentials.yaml")
+    if creds:
+        email = creds.get("email", None)  # type: ignore
+        password = creds.get("password", None)  # type: ignore
 
     return email, password
 
 
 def get_targets():
     # get page to search from external file called "targets"
-    with open(CONFIGDIR / "targets.yaml", "r") as targets:
-        try:
-            return yaml.safe_load(targets)
-        except yaml.YAMLError as exc:
-            logging.error(f"There was an error {exc}")
+    return yaml_loader(CONFIGDIR / "targets.yaml")["pages"]
